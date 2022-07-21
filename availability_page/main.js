d1fb5b84e98cb4811c94f6f7f3b3401ee824e17c
rahul <rkrahul8181@gmail.com>
2022-07-21 12:17:32 +0530

    day2

--- availability_page/main.js
+++ availability_page/main.js
@@ -1,13 +1,9 @@
+// ------- Import ------ hed and top section
+import { header, top } from "./components/help.js";
+document.querySelector('.head').innerHTML += header();
+document.querySelector('.top').innerHTML += top();
 
-// fetch('https://gist.githubusercontent.com/quarbby/5692cbd3e510df5ecd7cb7c093520dfc/raw/ebe47e8a31d9fa578a8011435cfcb69b5887695c/countries.json').then(res=>{
-//     res = res.json()
-//     console.log(res);
-//     return res
-//     // return res.json()
-// }).then(result=>{
-    // console.log(result)
-// })
-async function main(){
+async function main() {
     let url = `https://restcountries.com/v3.1/all`
     let res = await fetch(url);
     res = await res.json();
@@ -17,25 +13,25 @@ async function main(){
 // main();
 let cArr = [];
 
-let addCountries = (data)=>{
+let addCountries = (data) => {
     let box = document.getElementById('addCountries');
-    data.map(({name:{common},timezones})=>{
+    data.map(({ name: { common }, timezones }) => {
         // console.log(common,timezones[0])
         let div = document.createElement('div');
         let p1 = document.createElement('p');
-            p1.innerText = common;
+        p1.innerText = common;
         let p2 = document.createElement('p');
-            p2.innerText = timezones[0];
-        div.append(p1,p2);
-        div.addEventListener('mouseenter',(e)=>{
+        p2.innerText = timezones[0];
+        div.append(p1, p2);
+        div.addEventListener('mouseenter', (e) => {
             div.style.backgroundColor = "lightgray"
         });
-        div.addEventListener('mouseleave',()=>{
+        div.addEventListener('mouseleave', () => {
             div.style.backgroundColor = "white"
 
         });
-        div.addEventListener('click',()=>{
-            localStorage.setItem('c-name',common);
+        div.addEventListener('click', () => {
+            localStorage.setItem('c-name', common);
             document.getElementById('displayCountry').innerText = common;
             count--;
             document.getElementById('addCountries').style.display = "none";
@@ -62,15 +58,15 @@ document.getElementById('displayCountry').innerText = data;
 
 
 let countryBtn = document.getElementById('displayCountry');
-let count =0;
-countryBtn.addEventListener('click',()=>{
+let count = 0;
+countryBtn.addEventListener('click', () => {
     count++;
     // console.log('hi')
     let mainDiv = document.getElementById('addCountries');
-    if(count%2==1){
+    if (count % 2 == 1) {
         mainDiv.style.display = "block";
         main();
-    }else{
+    } else {
         mainDiv.style.display = "none";
     }
 
@@ -91,3 +87,26 @@ countryBtn.addEventListener('click',()=>{
 // }
 // console.log(subArr)
 // }
+
+// ------- adding input fields by using loop -------
+let inputFieldSet = () => {
+    let inputArr = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+    for (let i = 0; i < inputArr.length; i++) {
+        let inp = document.querySelector('.addInputTimes');
+        inp.innerHTML += `      <div class="input_time">
+        <div class="input_time_first">
+            <input type="checkbox" name="" id="">
+            <span>${inputArr[i]}</span>
+            <input type="text" name="" id="" value="9:00">
+            <span> - </span>
+            <input type="text" name="" id="" value="5:00">
+            <span><i class="fa-regular fa-trash-can"></i></span>
+        </div>
+        <div class="input_time_second">
+            <span>+</span>
+            <span><i class="fa-regular fa-copy"></i></span>
+        </div>
+    </div>`
+    }
+}
+inputFieldSet();
